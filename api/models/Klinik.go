0eb3515d4062d7e47985986b880ae791172d062a
Yulianto Mustaqim <ymustaqim19@gmail.com>
2022-12-09 08:36:27 +0700

    detail klinik

--- api/models/Klinik.go
+++ api/models/Klinik.go
@@ -8,28 +8,22 @@
 package models
 
 import (
-	"fmt"
-
 	"github.com/jinzhu/gorm"
 )
 
 type Klinik struct {
-	ID     int32  `gorm:"primary_key;auto_increment" json:"id"`
-	Nama   string `gorm:"size:255;not null;" json:"nama"`
-	Alamat string `gorm:"size:255;not null;" json:"alamat"`
-	Status string `gorm:"size:255;not null;" json:"status"`
+	ID           int32  `gorm:"primary_key;auto_increment" json:"id"`
+	Nama         string `gorm:"size:255;not null;" json:"nama"`
+	Alamat       string `gorm:"size:255;not null;" json:"alamat"`
+	Status       string `gorm:"size:255;not null;" json:"status"`
+	Hari_mulai   string `gorm:"size:255;not null;" json:"hari_mulai"`
+	Hari_selesai string `gorm:"size:255;not null;" json:"hari_selesai"`
+	Jam_mulai    string `gorm:"size:255;not null;" json:"jam_mulai"`
+	Jam_selesai  string `gorm:"size:255;not null;" json:"jam_selesai"`
 }
 
 type jadwalKlinik struct {
-	ID          int32       `gorm:"primary_key;auto_increment" json:"id"`
-	Id_klinik   int32       `gorm:"size:255;null;" json:"Id_klinik"`
-	Jam_praktek interface{} `gorm:"size:255;null;" json:"Jam_praktek"`
-	Jam_selesai interface{} `gorm:"size:255;null;" json:"Jam_selesai"`
-	Hari        string      `gorm:"size:255;null;" json:"Hari"`
-	Shift       string      `gorm:"size:255;null;" json:"Shift"`
-	Status      string      `gorm:"size:255;null;" json:"Status"`
-	Interval    string      `gorm:"size:255;null;" json:"Interval"`
-	Created_at  string      `gorm:"size:255;null;" json:"created_at"`
+	Status string `gorm:"size:255;null;" json:"Status"`
 }
 
 func (u *Klinik) FindKlinik(db *gorm.DB) (*[]Klinik, error) {
@@ -47,16 +41,15 @@ func (u *Klinik) DetailKlinik(db *gorm.DB, id int32) (*[]Klinik, error) {
 	klinik := []Klinik{}
 	err = db.Debug().Select("klinik.id, klinik.nama, klinik.alamat, klinik.status,min(jadwal_klinik.hari) as hari_mulai, max(jadwal_klinik.hari) as hari_selesai, min(jadwal_klinik.jam_praktek) as jam_mulai, max(jadwal_klinik.jam_selesai) as jam_selesai").
 		Model(&Klinik{}).
-		Model(&jadwalKlinik{}).
 		Joins("JOIN jadwal_klinik on jadwal_klinik.id_klinik=klinik.id").
 		Where(&Klinik{Status: "Aktif"}).
 		Where(&jadwalKlinik{Status: "Aktif"}).
 		Where(&Klinik{ID: id}).
 		Limit(15).
 		Find(&klinik).Error
-	fmt.Println("err", &klinik)
 	if err != nil {
-		return &[]Klinik{}, err
+		return &klinik, err
 	}
+
 	return &klinik, err
 }
